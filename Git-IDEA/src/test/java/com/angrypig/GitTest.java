0f698cb670436fc6dc6a0af08eda22b35dcfd803
ihhn <101168744+ihhn@users.noreply.github.com>
2022-11-04 18:13:25 +0800

    我在GitHub上添加了一行代码
    我在github上添加了一行代码

--- Git-IDEA/src/test/java/com/angrypig/GitTest.java
+++ Git-IDEA/src/test/java/com/angrypig/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("第4次在IDEA中使用git..");
         System.out.println("hot-fix2");
+        System.out.println("这一行代码是在github上添加的...");
     }
 }
