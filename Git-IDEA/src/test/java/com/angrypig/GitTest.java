96274fb3a23d4ee2e1c88581a765cc42152f3b2d
angrypig <angrypig@gmail.com>
2022-11-04 16:47:56 +0800

    hot-fix commit

--- Git-IDEA/src/test/java/com/angrypig/GitTest.java
+++ Git-IDEA/src/test/java/com/angrypig/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("第1次在IDEA中使用git..");
         System.out.println("第2次在IDEA中使用git..");
         System.out.println("第3次在IDEA中使用git..");
+        System.out.println("hot-fix");
     }
 }
