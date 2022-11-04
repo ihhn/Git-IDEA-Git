f8933bf9bc0207ba0633b8089c609e2e123c39f3
angrypig <angrypig@gmail.com>
2022-11-04 16:35:24 +0800

    第2次在IDEA中使用git的commit功能

--- Git-IDEA/src/test/java/com/angrypig/GitTest.java
+++ Git-IDEA/src/test/java/com/angrypig/GitTest.java
@@ -3,5 +3,6 @@ package com.angrypig;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("第1次在IDEA中使用git..");
+        System.out.println("第2次在IDEA中使用git..");
     }
 }
